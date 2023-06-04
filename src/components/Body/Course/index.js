2c1fd738a1a41000be2dccfa1279cedf1d1be2be
chifung <1357954269@qq.com>
2023-06-04 17:14:15 +0800

    搜索

--- src/components/Body/Course/index.js
+++ src/components/Body/Course/index.js
@@ -1,11 +1,13 @@
 import './index.css';
 import Card from '../Card';
 import { useState, useEffect } from 'react';
-import { queryCourse } from '../../../api';
+import { queryCourse, searchCourse } from '../../../api';
 
 const list = ['最新', '最热', '活动'];
 
-function Course() {
+// Course组件
+// 接受参数props.searchKey
+function Course({ searchKey }) {
   // 响应式变量,相当于vue的data
   const [focusIndex, setFocusIndex] = useState(0);
   const [courseList, setCourseList] = useState([]);
@@ -23,30 +25,48 @@ function Course() {
     setCourseList(allCourses.slice(0, 10));
   };
 
-  /**
-   * 副作用钩子
-   * 第1个参数是副作用函数,也就是执行体/回调
-   * 弟2个参数是依赖项,[]的话这个副作用就相当于vue的mounted
-   * 这里意思就是进来的时候就获取数据
-   */
+  // 搜索数据
+  const searchData = async (title) => {
+    if (!title) return;
+    const res = await searchCourse({ title });
+    const allCourses = res.data;
+    setCourseList(allCourses.slice(0, 10));
+  };
+
+  // /**
+  //  * 副作用钩子
+  //  * 第1个参数是副作用函数,也就是执行体/回调
+  //  * 弟2个参数是依赖项,[]的话这个副作用就相当于vue的mounted
+  //  * 这里意思就是进来的时候就获取数据
+  //  */
+  // useEffect(() => {
+  //   // 初始化数据
+  //   refreshData();
+  // }, []);
+
   useEffect(() => {
-    // 初始化数据
+    if (searchKey) {
+      searchData(searchKey);
+      return;
+    }
+    setFocusIndex(0);
     refreshData();
-  }, []);
+  }, [searchKey]); // 依赖项, 当searchKey变化时会重新执行副作用
 
   return (
     <div>
       <div className='header-tags'>
         {/* 列表渲染 */}
-        {list.map((item, index) => (
-          <div
-            className={index === focusIndex ? 'item item-focus' : 'item'}
-            onClick={() => handleClick(index)}
-            key={index}
-          >
-            {item}
-          </div>
-        ))}
+        {!searchKey &&
+          list.map((item, index) => (
+            <div
+              className={index === focusIndex ? 'item item-focus' : 'item'}
+              onClick={() => handleClick(index)}
+              key={index}
+            >
+              {item}
+            </div>
+          ))}
       </div>
       <div className='courses'>
         {/* 列表渲染课程数据 */}
