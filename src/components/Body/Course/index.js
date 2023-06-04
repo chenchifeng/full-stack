e7338d82a7df8685a5c2bef6ee340f9895efbd32
chifung <1357954269@qq.com>
2023-06-04 22:01:42 +0800

    实现伪分页

--- src/components/Body/Course/index.js
+++ src/components/Body/Course/index.js
@@ -1,10 +1,15 @@
 import './index.css';
 import Card from '../Card';
 import { Spin } from 'antd';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { queryCourse, searchCourse } from '../../../api';
 
 const list = ['最新', '最热', '活动'];
+
+// 前端伪分页
+let allCourses = null;
+let offset = 0;
+const limit = 20;
 
 // Course组件
 // 接受参数props.searchKey
@@ -13,6 +18,8 @@ function Course({ searchKey }) {
   const [focusIndex, setFocusIndex] = useState(0);
   const [courseList, setCourseList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+
+  const loaderRef = useRef(null);
 
   // 类型点击事件
   const handleClick = async (index) => {
@@ -25,8 +32,10 @@ function Course({ searchKey }) {
     setIsLoading(true);
     const res = await queryCourse({ type });
     setIsLoading(false);
-    const allCourses = res.data;
-    setCourseList(allCourses.slice(0, 10));
+    allCourses = res.data;
+    offset = 0;
+    setCourseList(allCourses.slice(0, limit));
+    offset = offset + limit;
   };
 
   // 搜索数据
@@ -36,7 +45,9 @@ function Course({ searchKey }) {
     const res = await searchCourse({ title });
     setIsLoading(false);
     const allCourses = res.data;
-    setCourseList(allCourses.slice(0, 10));
+    offset = 0;
+    setCourseList(allCourses.slice(0, limit));
+    offset = offset + limit;
   };
 
   // /**
@@ -58,6 +69,49 @@ function Course({ searchKey }) {
     setFocusIndex(0);
     refreshData();
   }, [searchKey]); // 依赖项, 当searchKey变化时会重新执行副作用
+
+  // 利用IntersectionObserver观察下面创建并且绑定的那个div(loaderRef)是否进入视口来实现判断是否触底
+  // 实现触底加载更多
+  useEffect(() => {
+    // 定义变量保存loaderRef.current,避免在清理副作用的时候受到oaderRef.current变化的影响
+    const loaderRefCurrent = loaderRef.current
+    // IntersectionObserver 是一个浏览器原生的 API，可以用于观察元素是否进入或离开视口。
+    // 在这段代码中，它的配置选项 rootMargin 设置为 '0px 0px 100% 0px'
+    // 意味着当 loaderRef.current 元素进入视口底部 100% 的时候，就会触发回调函数 moreData()。
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          moreData();
+        }
+      },
+      { rootMargin: '0px 0px 100% 0px' },
+    );
+    if (loaderRef.current) {
+      observer.observe(loaderRef.current);
+    }
+    return () => {
+      // 清理副作用函数
+      // 组件卸载前取消IntersectionObserver监听,避免内存泄漏
+      if (loaderRefCurrent) {
+        observer.unobserve(loaderRefCurrent);
+      }
+    };
+  }, []); // 只要 loaderRef.current 发生变化，就需要重新创建一个 IntersectionObserver 对象并进行监听
+
+  /**
+   * 加载更多数据
+   * 伪分页
+   * 实际上的分页应该是把新的数据拼在后面
+   * 这里的分页是直接把已经全部获取到的数据,进行新的截取赋值
+   */
+  const moreData = () => {
+    console.log('获取更多数据');
+    if (!allCourses || offset >= allCourses.length) {
+      return;
+    }
+    setCourseList(allCourses.slice(0, offset + limit));
+    offset = offset + limit;
+  };
 
   return (
     <div>
@@ -82,6 +136,8 @@ function Course({ searchKey }) {
           ))}
         </div>
       </Spin>
+      {/*  */}
+      <div ref={loaderRef} />
     </div>
   );
 }
